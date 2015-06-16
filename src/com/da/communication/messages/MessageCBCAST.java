09bbd6f469d3cc1eb9347b0f61ee2bca29dcb5e9
Daniel Colceag <colceagdaniel@github.com>
2015-06-16 11:07:47 +0300

    Reformat code on MessageCBCAST

--- src/com/da/communication/messages/MessageCBCAST.java
+++ src/com/da/communication/messages/MessageCBCAST.java
@@ -1,47 +1,47 @@
 package com.da.communication.messages;
+
+import com.da.communication.CommModule;
+
 import java.util.ArrayList;
-import java.util.Date;
 
-import com.da.communication.*;
+public class MessageCBCAST extends Message implements java.io.Serializable {
+    private static final long serialVersionUID = -5212804843246099826L;
+    // Vector TimeStamp
+    public ArrayList<Integer> vt;
+    public int vtlen = CommModule.connections.size() + 1;
 
-public class MessageCBCAST extends Message implements java.io.Serializable{
-	private static final long serialVersionUID = -5212804843246099826L;
-	// Vector TimeStamp
-	public ArrayList<Integer> vt;
-	public int vtlen =  CommModule.connections.size() + 1;
+    public MessageCBCAST() {
+        super();
+        vtlen = CommModule.connections.size() + 1;
+        this.vt = new ArrayList<Integer>(vtlen);
+        for (int i = 0; i < vtlen; i++) {
+            this.vt.add(0);
+        }
+    }
 
-	public MessageCBCAST() {
-		super();
-		vtlen = CommModule.connections.size() + 1;
-		this.vt = new ArrayList<Integer>(vtlen);
-		for (int i = 0; i < vtlen; i++){
-			this.vt.add(0);
-		}
-	}
-	
-	public MessageCBCAST(Operation op) {
-		this();
-		setOperation(op);
-	}
-	
-	public MessageCBCAST(Operation op, ArrayList<Integer> timestamp){
-		this(op);
-		this.vt = new ArrayList<Integer>(timestamp);
-	}
-	
-	public void setVT(ArrayList<Integer> timestamp){
-		this.vt = new ArrayList<Integer>(timestamp);
-	}
-	
-	@Override
-	public String toString(){
-		String svt = "[" + vt.get(0);
-		
-		for (int i = 1; i < vt.size(); i++){
-			svt += "," + vt.get(i).toString();
-		}
-		
-		svt += "]";
-		return "(" + getOperation().toString() + "," + svt + ")";
-	}
+    public MessageCBCAST(Operation op) {
+        this();
+        setOperation(op);
+    }
+
+    public MessageCBCAST(Operation op, ArrayList<Integer> timestamp) {
+        this(op);
+        this.vt = new ArrayList<Integer>(timestamp);
+    }
+
+    public void setVT(ArrayList<Integer> timestamp) {
+        this.vt = new ArrayList<Integer>(timestamp);
+    }
+
+    @Override
+    public String toString() {
+        String svt = "[" + vt.get(0);
+
+        for (int i = 1; i < vt.size(); i++) {
+            svt += "," + vt.get(i).toString();
+        }
+
+        svt += "]";
+        return "(" + getOperation().toString() + "," + svt + ")";
+    }
 }
